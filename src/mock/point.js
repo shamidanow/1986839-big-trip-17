0da540143178ee4a8ef1f560d945fb82a319218d
shamidanow <shamidanow@users.noreply.github.com>
2022-05-02 15:56:30 +0500

    Исправит отрицательную продолжительность события

--- src/mock/point.js
+++ src/mock/point.js
@@ -5,8 +5,8 @@ import {generateOffer} from './offer';
 import {generateDestination} from './destination';
 import dayjs from 'dayjs';
 
-const generateDate = () => {
-  const daysGap = getRandomInteger(0, 7);
+const generateDate = (dayBegin, dayEnd) => {
+  const daysGap = getRandomInteger(dayBegin, dayEnd);
   const hoursGap = getRandomInteger(0, 24);
   const minutesGap = getRandomInteger(0, 60);
   const secondsGap = getRandomInteger(0, 60);
@@ -17,8 +17,8 @@ const generateDate = () => {
 export const generatePoint = () => ({
   id: getRandomInteger(1, 10),
   basePrice: getRandomInteger(100, 1100),
-  dateFrom: generateDate(),
-  dateTo: generateDate(),
+  dateFrom: generateDate(0, 2),
+  dateTo: generateDate(3, 5),
   destination: getRandomArrayElement(Array.from({length: 10}, generateDestination)),
   isFavorite: Boolean(getRandomInteger(0, 1)),
   offers: getRandomArrayElement(Array.from({length: 20}, generateOffer)).offers,
