2a688107f19d29088f246a4bb8fe10f439968de9
shamidanow <shamidanow@users.noreply.github.com>
2022-05-03 13:34:04 +0500

    Добавит компонентам приватные поля и геттеры

--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -6,22 +6,27 @@ import EventEditView from '../view/event-edit-view';
 import EventView from '../view/event-view';
 
 export default class EventPresenter {
-  eventComponent = new EventSectionView();
-  eventListComponent = new EventListView();
+  #eventContainer = null;
+  #pointsModel = null;
+
+  #eventComponent = new EventSectionView();
+  #eventListComponent = new EventListView();
+
+  #eventPoints = [];
 
   init = (eventContainer, pointsModel) => {
-    this.eventContainer = eventContainer;
-    this.pointsModel = pointsModel;
-    this.eventPoints = [...this.pointsModel.getPoints()];
+    this.#eventContainer = eventContainer;
+    this.#pointsModel = pointsModel;
+    this.#eventPoints = [...this.#pointsModel.points];
 
-    render(this.eventComponent, this.eventContainer);
-    render(new SortView(), this.eventComponent.getElement());
+    render(this.#eventComponent, this.#eventContainer);
+    render(new SortView(), this.#eventComponent.element);
 
-    render(this.eventListComponent, this.eventComponent.getElement());
-    render(new EventEditView(this.eventPoints[0]), this.eventListComponent.getElement());
+    render(this.#eventListComponent, this.#eventComponent.element);
+    render(new EventEditView(this.#eventPoints[0]), this.#eventListComponent.element);
 
-    for (let i = 1; i < this.eventPoints.length; i++) {
-      render(new EventView(this.eventPoints[i]), this.eventListComponent.getElement());
+    for (let i = 1; i < this.#eventPoints.length; i++) {
+      render(new EventView(this.#eventPoints[i]), this.#eventListComponent.element);
     }
   };
 }
--- src/view/info-main-view.js
+++ src/view/info-main-view.js
@@ -9,19 +9,21 @@ const createInfoMainTemplate = () => (
 );
 
 export default class InfoMainView {
-  getTemplate() {
+  #element = null;
+
+  get template() {
     return createInfoMainTemplate();
   }
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
     }
 
-    return this.element;
+    return this.#element;
   }
 
   removeElement() {
-    this.element = null;
+    this.#element = null;
   }
 }
